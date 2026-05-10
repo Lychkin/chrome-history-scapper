6fe31d5b21048fb6abee7535a055aa9e4e8f6716
Lychkin <slavochka.lychkin@mail.ru>
2026-05-10 22:47:05 +0300

    add runtime logs, changed plot to calplot, refactoring

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import datetime
 import sqlite3
 import pandas as pd
 import matplotlib.pyplot as plt
-import matplotlib.dates as mdates
+import calplot
 
 
 def find_history_dbs():
@@ -29,11 +29,13 @@ db_files = find_history_dbs()
 
 df = pd.DataFrame()
 for db_file in db_files:
+    print(f"Connecting to {db_file}...")
     conn = sqlite3.connect(db_file)
     df = pd.concat([df, pd.read_sql("SELECT * FROM urls", conn)])
     conn.close()
 
 # Dataframe сформирован, теперь обработаем его
+print("Entries before proccesing: ", len(df))
 df.drop_duplicates(inplace=True)
 
 df = df[df["last_visit_time"] != 0]
@@ -48,37 +50,42 @@ df.reset_index(inplace=True)
 df["last_visit_time"] = df["last_visit_time"].map(date_from_webkit)
 
 
-print(len(df))
-print(df.columns)
+print("Entries after proccesing: ", len(df))
 df.info()
 
-# Dataframe обработан, сделаем экспорт и график
+# Dataframe обработан, сделаем экспорт в таблицу Excel и график
 
 os.makedirs("data", exist_ok=True)
 
-events = df["last_visit_time"].tolist()
-
 now = datetime.datetime.now().strftime("%d-%m-%y %H-%M-%S")
 
+# Экспорт в Excel-таблицу
 merged_table_name = f"data/history_{now}.xlsx"
 df.to_excel(merged_table_name, index=False)
+print("Export to Excel - done")
 
-fig, ax = plt.subplots(figsize=(10, 2))
-
-ax.eventplot(
-    events,
-    orientation="horizontal",
-    colors="green",
-    lineoffsets=0,
-    linelengths=1.5,
+# Экспорт в график внутри Pdf-файла
+df_dates = df[["last_visit_time"]].copy()
+df_dates["last_visit_time"] = df_dates["last_visit_time"].dt.normalize()
+df_dates = (
+    df_dates.groupby(df["last_visit_time"].dt.normalize())
+    .size()
+    .reset_index(name="count")
 )
 
-ax.xaxis.set_major_formatter(mdates.DateFormatter("%Y-%B"))
-plt.xticks(rotation=45)
-ax.get_yaxis().set_visible(False)
+df_dates = df_dates.set_index("last_visit_time")
+data = df_dates["count"]
 
-plt.title("История - даты активности")
-plt.tight_layout()
+calplot.calplot(
+    data,
+    cmap="YlGnBu",
+    figsize=(16, 10),
+    yearlabel_kws={"fontname": "Arial", "fontsize": 18},
+    subplot_kws={"title": ""},
+    suptitle="Calendar Heatmap по годам",
+)
 
 plot_name = f"data/history_{now}.pdf"
 plt.savefig(plot_name, dpi=300)
+
+print("Export to PDF - done")
